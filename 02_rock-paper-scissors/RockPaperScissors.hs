90a827ad5cfdd56751a36645d37a04b2c5f0ddc6
Scriptim <me@scriptim.dev>
2022-12-02 10:52:25 +0100

    Add solution for day 2, part 2

--- 02_rock-paper-scissors/RockPaperScissors.hs
+++ 02_rock-paper-scissors/RockPaperScissors.hs
@@ -2,20 +2,25 @@ module RockPaperScissors (parseInput, part1, part2) where
 
 import AdventOfCode (Parser)
 import Text.Megaparsec (some, (<|>))
-import Text.Megaparsec.Char (char, newline)
+import Text.Megaparsec.Char (char, newline, space)
 
 data Shape = Rock | Paper | Scissors
 
-parseInput :: Parser [(Shape, Shape)]
-parseInput = some parsePair
+data Outcome = Loss | Draw | Win deriving (Eq)
+
+data ShapeOrOutcome = ShapeOrOutcome Shape Outcome
+
+parseInput :: Parser [(Shape, ShapeOrOutcome)]
+parseInput = some parseRound
   where
-    parsePair = do
+    parseRound = do
       opp <- shape
-      _ <- char ' '
-      self <- shape
+      _ <- space
+      self <- shapeOrOutcome
       _ <- newline
       return (opp, self)
-    shape = Rock <$ char 'A' <|> Paper <$ char 'B' <|> Scissors <$ char 'C' <|> Rock <$ char 'X' <|> Paper <$ char 'Y' <|> Scissors <$ char 'Z'
+    shape = Rock <$ char 'A' <|> Paper <$ char 'B' <|> Scissors <$ char 'C'
+    shapeOrOutcome = ShapeOrOutcome Rock Loss <$ char 'X' <|> ShapeOrOutcome Paper Draw <$ char 'Y' <|> ShapeOrOutcome Scissors Win <$ char 'Z'
 
 beats :: Shape -> Shape -> Bool
 beats Rock Scissors = True
@@ -23,22 +28,33 @@ beats Paper Rock = True
 beats Scissors Paper = True
 beats _ _ = False
 
+outcome :: Shape -> Shape -> Outcome
+outcome opp self
+  | beats opp self = Loss
+  | beats self opp = Win
+  | otherwise = Draw
+
 shapeScore :: Shape -> Int
 shapeScore Rock = 1
 shapeScore Paper = 2
 shapeScore Scissors = 3
 
-outcomeScore :: Shape -> Shape -> Int
-outcomeScore opp self
-  | beats opp self = 0
-  | beats self opp = 6
-  | otherwise = 3
+outcomeScore :: Outcome -> Int
+outcomeScore Loss = 0
+outcomeScore Draw = 3
+outcomeScore Win = 6
 
-roundScore :: Shape -> Shape -> Int
-roundScore opp self = shapeScore self + outcomeScore opp self
+action :: Shape -> Outcome -> Shape
+action opp outc = head $ filter ((==) outc . outcome opp) [Rock, Paper, Scissors]
 
-part1 :: [(Shape, Shape)] -> String
-part1 = show . sum . map (uncurry roundScore)
+roundScore1 :: Shape -> ShapeOrOutcome -> Int
+roundScore1 opp (ShapeOrOutcome self _) = shapeScore self + outcomeScore (outcome opp self)
 
-part2 :: [(Shape, Shape)] -> String
-part2 = undefined
+roundScore2 :: Shape -> ShapeOrOutcome -> Int
+roundScore2 opp (ShapeOrOutcome _ outc) = shapeScore (action opp outc) + outcomeScore outc
+
+part1 :: [(Shape, ShapeOrOutcome)] -> String
+part1 = show . sum . map (uncurry roundScore1)
+
+part2 :: [(Shape, ShapeOrOutcome)] -> String
+part2 = show . sum . map (uncurry roundScore2)
