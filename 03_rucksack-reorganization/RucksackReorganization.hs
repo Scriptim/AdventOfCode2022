d3aa74ae11fdc9fcaaaee1b4004d8ef15341a235
Scriptim <me@scriptim.dev>
2022-12-03 14:57:25 +0100

    Add solution for day 3, part 2

--- 03_rucksack-reorganization/RucksackReorganization.hs
+++ 03_rucksack-reorganization/RucksackReorganization.hs
@@ -2,6 +2,7 @@ module RucksackReorganization (parseInput, part1, part2) where
 
 import AdventOfCode (Parser)
 import Data.List (intersect)
+import Data.List.Extra (chunksOf)
 import Data.Maybe (fromJust)
 import Text.Megaparsec (endBy, some)
 import Text.Megaparsec.Char (letterChar, newline)
@@ -22,8 +23,14 @@ commonItem xs ys = head $ intersect xs ys
 priority :: Item -> Int
 priority item = fromJust . lookup item $ zip (['a' .. 'z'] ++ ['A' .. 'Z']) [1 .. 52]
 
+groups :: [Rucksack] -> [[Rucksack]]
+groups = chunksOf 3
+
+badgeItem :: [Rucksack] -> Item
+badgeItem = head . foldr1 intersect
+
 part1 :: [Rucksack] -> String
 part1 = show . sum . map (priority . uncurry commonItem . compartments)
 
 part2 :: [Rucksack] -> String
-part2 = const "Not implemented"
+part2 = show . sum . map (priority . badgeItem) . groups
