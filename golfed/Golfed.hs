607049dff65f8047a1eefccc710ed83c67a4b3a4
Scriptim <me@scriptim.dev>
2022-12-05 12:20:43 +0100

    Add golfed solutions for day 5

--- golfed/Golfed.hs
+++ golfed/Golfed.hs
@@ -1,12 +1,14 @@
 {-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
 {-# OPTIONS_GHC -Wno-missing-export-lists #-}
 {-# OPTIONS_GHC -Wno-missing-signatures #-}
+{-# OPTIONS_GHC -Wno-name-shadowing #-}
 {-# OPTIONS_GHC -Wno-type-defaults #-}
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
 
 import Data.Char (ord)
-import Data.List (elemIndex, intersect, sort)
-import Data.List.Split (splitOn, chunksOf)
+import Data.List (elemIndex, intersect, sort, transpose)
+import Data.List.Extra (trim)
+import Data.List.Split (chunksOf, splitOn)
 
 -- main :: IO ()
 -- main = dayXXpartX
@@ -26,3 +28,7 @@ day03part2 = print . sum . map (maybe 0 succ . (`elemIndex` (['a' .. 'z'] ++ ['A
 day04part1 = print . length . filter (\[[a,b],[c,d]] -> a<=c&&b>=d||c<=a&&d>=b) . map (map (map (read::String->Int) . splitOn "-") . splitOn ",") . lines =<< readFile "04_camp-cleanup/input.txt"
 
 day04part2 = print . length . filter (\[[a,b],[c,d]] -> b>=c&&a<=d||d>=a&&c<=b) . map (map (map (read::String -> Int) . splitOn "-") . splitOn ",") . lines =<< readFile "04_camp-cleanup/input.txt"
+
+day05part1 = putStrLn . map head . uncurry (foldl (\s (n,f,t) -> let a f n xs = take n xs ++ f (xs !! n) : drop (n + 1) xs in a (drop n) f $ a ((reverse . take n $ s !! f) ++) t s)) . (\[a,b] -> (map trim . transpose . map (map (!!1) . chunksOf 4) . init $ a, map ((\[_,n,_,f,_,t] -> (read n, pred $ read f, pred $ read t)) . splitOn " ") b)) . map lines . splitOn "\n\n" =<< readFile "05_supply-stacks/input.txt"
+
+day05part2 = putStrLn . map head . uncurry (foldl (\s (n,f,t) -> let a f n xs = take n xs ++ f (xs !! n) : drop (n + 1) xs in a (drop n) f $ a ((take n $ s !! f) ++) t s)) . (\[a,b] -> (map trim . transpose . map (map (!!1) . chunksOf 4) . init $ a, map ((\[_,n,_,f,_,t] -> (read n, pred $ read f, pred $ read t)) . splitOn " ") b)) . map lines . splitOn "\n\n" =<< readFile "05_supply-stacks/input.txt"
