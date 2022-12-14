43da110a385045e46bd90dfb7c8a119be98426e7
Scriptim <me@scriptim.dev>
2022-12-14 18:30:13 +0100

    Add solution for day 14, part 2

--- 14_regolith-reservoir/RegolithReservoir.hs
+++ 14_regolith-reservoir/RegolithReservoir.hs
@@ -17,24 +17,26 @@ parseInput = S.fromList . concatMap structureCoordinates <$> structureEndpoints 
     structureEndpoints = coordinate `sepBy` string (pack " -> ") :: Parser [Coordinate]
     coordinate = (,) <$> decimal <* char ',' <*> decimal :: Parser Coordinate
 
-landingPosition :: S.Set Coordinate -> Int -> Coordinate -> Maybe Coordinate
-landingPosition blocks abyss (x, y)
-  | y > abyss = Nothing
-  | S.notMember (x, succ y) blocks = landingPosition blocks abyss (x, succ y)
-  | S.notMember (pred x, succ y) blocks = landingPosition blocks abyss (pred x, succ y)
-  | S.notMember (succ x, succ y) blocks = landingPosition blocks abyss (succ x, succ y)
+landingPosition :: S.Set Coordinate -> Bool -> Int -> Coordinate -> Maybe Coordinate
+landingPosition blocks isAbyss abyssOrFloor (x, y)
+  | isAbyss && y >= abyssOrFloor = Nothing
+  | not isAbyss && succ y == abyssOrFloor = Just (x, y)
+  | S.notMember (x, succ y) blocks = landingPosition blocks isAbyss abyssOrFloor (x, succ y)
+  | S.notMember (pred x, succ y) blocks = landingPosition blocks isAbyss abyssOrFloor (pred x, succ y)
+  | S.notMember (succ x, succ y) blocks = landingPosition blocks isAbyss abyssOrFloor (succ x, succ y)
   | otherwise = Just (x, y)
 
-produceSand :: S.Set Coordinate -> S.Set Coordinate
-produceSand blocks = go blocks
+produceSand :: S.Set Coordinate -> Bool -> S.Set Coordinate
+produceSand blocks isAbyss = go blocks
   where
-    abyss = maximum . map snd . S.toList $ blocks
-    go blocks' = case landingPosition blocks' abyss (500, 0) of
+    abyss = succ . maximum . map snd . S.toList $ blocks
+    floorLine = succ abyss
+    go blocks' = case landingPosition blocks' isAbyss (if isAbyss then abyss else floorLine) (500, 0) of
       Nothing -> blocks'
-      Just pos -> go $ S.insert pos blocks'
+      Just pos -> (if snd pos == 0 then id else go) $ S.insert pos blocks'
 
 part1 :: S.Set Coordinate -> String
-part1 rocks = show $ S.size (produceSand rocks) - S.size rocks
+part1 rocks = show $ S.size (produceSand rocks True) - S.size rocks
 
 part2 :: S.Set Coordinate -> String
-part2 = undefined
+part2 rocks = show $ S.size (produceSand rocks False) - S.size rocks
