2c66cb813016cd3a6f83da7adf1627ab68f431c0
Scriptim <me@scriptim.dev>
2022-12-22 23:00:54 +0100

    Add solution for day 20, part 2

--- 20_grove-positioning-system/GrovePositioningSystem.hs
+++ 20_grove-positioning-system/GrovePositioningSystem.hs
@@ -1,6 +1,7 @@
 module GrovePositioningSystem (parseInput, part1, part2) where
 
 import AdventOfCode (Parser)
+import Control.Monad (ap)
 import Data.Maybe (fromJust)
 import qualified Data.Sequence as S
 import Text.Megaparsec (endBy)
@@ -17,19 +18,20 @@ move list n = S.insertAt newIndex element . S.deleteAt index $ list
     element@(_, value) = S.index list index
     newIndex = (index + value) `mod` pred (S.length list)
 
-mix :: S.Seq Int -> S.Seq Int
-mix xs = snd . S.unzip $ foldl move (S.zip (S.fromList indices) xs) indices
-  where
-    indices = [0 .. S.length xs - 1]
+rememberIndices :: S.Seq Int -> S.Seq (Int, Int)
+rememberIndices = S.zip =<< S.fromList . enumFromTo 0 . pred . S.length
 
-groveCoordinatesSum :: S.Seq Int -> Int
+mix :: S.Seq (Int, Int) -> S.Seq (Int, Int)
+mix = ap (foldl move) (enumFromTo 0 . pred . S.length)
+
+groveCoordinatesSum :: S.Seq (Int, Int) -> Int
 groveCoordinatesSum list = nth 1000 + nth 2000 + nth 3000
   where
-    zero = fromJust $ S.findIndexL (== 0) list
-    nth n = S.index list $ (zero + n) `mod` S.length list
+    zero = fromJust $ S.findIndexL ((== 0) . snd) list
+    nth n = snd . S.index list $ (zero + n) `mod` S.length list
 
 part1 :: [Int] -> String
-part1 = show . groveCoordinatesSum . mix . S.fromList
+part1 = show . groveCoordinatesSum . mix . rememberIndices . S.fromList
 
 part2 :: [Int] -> String
-part2 = undefined
+part2 = show . groveCoordinatesSum . (!! 10) . iterate mix . rememberIndices . fmap (* 811589153) . S.fromList
